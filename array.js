9750bb00c1e25d36958a852b89d9db74805b041c
Muhammad usama khan <161209226+usamakhan2222@users.noreply.github.com>
2024-05-26 15:42:34 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,20 @@
+var people = {
+    friends: []
+};
+var friend1 = {
+    id: 1,
+    firstname: "amin",
+    lastname: "alam",
+};
+var friend2 = {
+    id: 2,
+    firstname: "usama",
+    lastname: "khan",
+};
+var friend3 = {
+    id: 3,
+    firstname: "usama",
+    lastname: "jamal",
+};
+people.friends.push(friend1, friend2, friend3);
+console.log(people);
---
+++ array.ts
@@ -0,0 +1,20 @@
+const people = {
+    friends:[]
+};
+const friend1={
+    id:1,
+    firstname:"amin",
+    lastname:"alam",
+};
+const friend2={
+    id:2,
+    firstname:"usama",
+    lastname:"khan",
+};
+const friend3 ={
+id:3,
+firstname:"usama",
+lastname:"jamal",
+};
+people.friends.push (friend1, friend2, friend3);
+console.log(people);
